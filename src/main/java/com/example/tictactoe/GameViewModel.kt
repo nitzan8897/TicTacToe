e8324d06e961046bf04808e7a223cce032349a75
Arbel Charny <arbel.charny@gmail.com>
2026-01-22 18:07:01 +0200

    Added Game Logic

---
+++ src/main/java/com/example/tictactoe/GameViewModel.kt
@@ -0,0 +1,70 @@
+package com.example.tictactoe
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+enum class Player { X, O, NONE }
+
+data class GameState(
+    val board: List<List<Player>>,
+    val currentPlayer: Player,
+    val winner: Player?,
+    val isDraw: Boolean,
+    val isGameOver: Boolean
+)
+
+class GameViewModel : ViewModel() {
+
+    private val _gameState = MutableStateFlow(createInitialState())
+    val gameState: StateFlow<GameState> = _gameState.asStateFlow()
+
+    fun onCellClicked(row: Int, col: Int) {
+        val currentState = _gameState.value
+        if (currentState.isGameOver || currentState.board[row][col] != Player.NONE) return
+
+        val newBoard = currentState.board.map { it.toMutableList() }
+        newBoard[row][col] = currentState.currentPlayer
+
+        val winner = checkVictory(newBoard)
+        val isDraw = winner == null && newBoard.flatten().none { it == Player.NONE }
+        val isGameOver = winner != null || isDraw
+        val nextPlayer = if (currentState.currentPlayer == Player.X) Player.O else Player.X
+
+        _gameState.value = currentState.copy(
+            board = newBoard,
+            currentPlayer = if (isGameOver) currentState.currentPlayer else nextPlayer,
+            winner = winner,
+            isDraw = isDraw,
+            isGameOver = isGameOver
+        )
+    }
+
+    fun resetGame() {
+        _gameState.value = createInitialState()
+    }
+
+    private fun createInitialState(): GameState = GameState(
+        board = List(3) { List(3) { Player.NONE } },
+        currentPlayer = Player.X,
+        winner = null,
+        isDraw = false,
+        isGameOver = false
+    )
+
+    private fun checkVictory(board: List<List<Player>>): Player? {
+        for (i in 0..2) {
+            if (board[i][0] != Player.NONE && board[i][0] == board[i][1] && board[i][1] == board[i][2]) return board[i][0]
+        }
+
+        for (i in 0..2) {
+            if (board[0][i] != Player.NONE && board[0][i] == board[1][i] && board[1][i] == board[2][i]) return board[0][i]
+        }
+
+        if (board[0][0] != Player.NONE && board[0][0] == board[1][1] && board[1][1] == board[2][2]) return board[0][0]
+        if (board[0][2] != Player.NONE && board[0][2] == board[1][1] && board[1][1] == board[2][0]) return board[0][2]
+
+        return null
+    }
+}
